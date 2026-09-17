f44deec5861bd4eb403dd44285ce216a307e2205
Eltaf-azizi <iltfhuss@gmail.com>
2026-09-17 22:33:05 +0500

    working on the init

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+RAG Constitutional QA System
+
+A Retrieval-Augmented Generation system for answering questions
+about constitutional documents with grounded citations.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
